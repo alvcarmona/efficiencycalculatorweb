ed1a008d1e284d3bbf6bceef330a50ea5c6100b2
Alvaro Carmona Basanez <alvarocbasanez@ci0011810.esss.lu.se>
2017-09-19 13:24:17 +0200

    changed package.json and node config

--- effcalculator/effcalculator/settings.py
+++ effcalculator/effcalculator/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/frontend/assets/'
 
 STATICFILES_DIRS = (
     #This lets Django's collectstatic store our bundles
